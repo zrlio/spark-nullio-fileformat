52dbfd0a2135d1a6564b1514924f747759bcd9f8
Animesh Trivedi <animesh.trivedi@gmail.com>
2017-08-10 17:17:22 +0200

    adding license and optimize imports

--- src/main/scala/com/ibm/crail/spark/sql/datasources/NullioFileFormat.scala
+++ src/main/scala/com/ibm/crail/spark/sql/datasources/NullioFileFormat.scala
@@ -1,3 +1,24 @@
+/*
+ * spark-nullio file format
+ *
+ * Author: Animesh Trivedi <atr@zurich.ibm.com>
+ *
+ * Copyright (C) 2017, IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package com.ibm.crail.spark.sql.datasources
 
 import com.ibm.crail.spark.sql.datasources.schema.{IntWithPayloadSchema, ParquetExampleGenerator, ParquetExampleSchema}
@@ -5,16 +26,11 @@ import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FileStatus, Path}
 import org.apache.hadoop.mapreduce.{Job, TaskAttemptContext}
 import org.apache.spark.SparkEnv
-import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.catalyst.InternalRow
-import org.apache.spark.sql.catalyst.expressions.UnsafeRow
-import org.apache.spark.sql.catalyst.expressions.codegen.{BufferHolder, UnsafeRowWriter}
 import org.apache.spark.sql.execution.datasources.{FileFormat, OutputWriter, OutputWriterFactory, PartitionedFile}
 import org.apache.spark.sql.sources.{DataSourceRegister, Filter}
 import org.apache.spark.sql.types._
-import org.apache.spark.unsafe.types.UTF8String
-
-import scala.util.Random
 
 object NullioFileFormat {
   val KEY_INPUT_ROWS = "inputrows"
